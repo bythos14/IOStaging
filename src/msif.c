5f9a264d1cb479b4b99356cd7a2732d096a680f2
bythos14 <45720552+bythos14@users.noreply.github.com>
2023-10-21 16:50:09 -0400

    Implement MSIF overclocking

--- src/msif.c
+++ src/msif.c
@@ -8,6 +8,9 @@
 
 #include "common.h"
 
+// Set to 1 to enable overclocking the Memory Stick to 60MHz
+#define OVERCLOCK_MSIF (1)
+
 typedef struct SceMsifAdmaDescriptor
 {
     SceUIntPtr addr;
@@ -27,8 +30,8 @@ typedef struct MsifStagingBuffer
     uint32_t *alignSizes;
 } MsifStagingBuffer;
 
-static tai_hook_ref_t hookRefs[5];
-static SceUID hookIds[5];
+static tai_hook_ref_t hookRefs[6];
+static SceUID hookIds[6];
 static MsifStagingBuffer stagingBuf;
 
 int module_get_offset(SceUID pid, SceUID modid, int segidx, size_t offset, uintptr_t *addr);
@@ -146,9 +149,34 @@ static int _msproal_write_sectors(void *pCtx, SceUInt32 sector, SceUInt32 count,
     return ret;
 }
 
+void kscePervasiveMsifSetClock(int clock);
+int kscePervasiveRemovableMemoryGetCardInsertState();
+
+static void SetMsifClock(void *pCtx, SceUInt32 unk)
+{
+    SceUInt16 regValue = *(SceUInt16 *)(*(void **)(pCtx + 0xC00) + 0xC);
+
+    if (unk == 1)
+        regValue = (regValue & ~0xC0) | 0x80; // Serial
+    else if (unk == 5)
+        regValue = (regValue & ~0xC0) | 0x40; // PAR8
+    else
+        regValue = regValue & ~0xC0;          // PAR4
+
+    *(SceUInt16 *)(*(void **)(pCtx + 0xC00) + 0xC) = regValue;
+
+    if (unk == 1)
+        kscePervasiveMsifSetClock(4); // Observed ~16MHz with 8 MB/s speed
+    else if (((unk - 2) < 3) && !OVERCLOCK_MSIF)
+        kscePervasiveMsifSetClock(5); // Observed ~32MHz with 16 MB/s speed
+    else
+        kscePervasiveMsifSetClock(6); // Observed ~40MHz with 20 MB/s speed
+}
+
 int InitMsifStaging()
 {
     SceMsifAdmaDescriptor **descAreaBase;
+    void **msifContext;
     tai_module_info_t moduleInfo = {0};
 
     moduleInfo.size = sizeof(moduleInfo);
@@ -162,12 +190,17 @@ int InitMsifStaging()
     hookIds[2] = taiHookFunctionOffsetForKernel(KERNEL_PID, &hookRefs[2], moduleInfo.modid, 0, 0x38F0, 1, _sceMsifPrepareDmaTable);
     hookIds[3] = taiHookFunctionOffsetForKernel(KERNEL_PID, &hookRefs[3], moduleInfo.modid, 0, 0xDDC, 1, _msproal_read_sectors);
     hookIds[4] = taiHookFunctionOffsetForKernel(KERNEL_PID, &hookRefs[4], moduleInfo.modid, 0, 0x107C, 1, _msproal_write_sectors);
+    hookIds[5] = taiHookFunctionOffsetForKernel(KERNEL_PID, &hookRefs[5], moduleInfo.modid, 0, 0x25C0, 1, SetMsifClock);
 
     module_get_offset(KERNEL_PID, moduleInfo.modid, 1, 0x14E4, (uintptr_t *)&stagingBuf.alignSizes);
     module_get_offset(KERNEL_PID, moduleInfo.modid, 1, 0x14F8, (uintptr_t *)&descAreaBase);
     stagingBuf.descArea = *descAreaBase;
 
     ksceKernelVAtoPA(stagingBuf.descArea, &stagingBuf.descAreaPAddr);
+
+    module_get_offset(KERNEL_PID, moduleInfo.modid, 1, 0x0, (uintptr_t *)&msifContext);
+    if ((*msifContext != NULL) && (*(SceUInt32 *)(*msifContext + 0xC0C) & 0x1)) // Check to see if memory stick is inserted and mounted already
+        kscePervasiveMsifSetClock(6);
 
     return 0;
 }
